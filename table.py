b5d7a300a021e5f4f5e671e68b013ea0c60c2f33
george ifantis <georgeifa13@gmail.com>
2021-01-07 17:34:56 +0200

    INLJ DEBUG

--- table.py
+++ table.py
@@ -531,11 +531,11 @@ class Table:
         # get columns and operator
         column_name_left, operator, column_name_right = self._parse_condition(condition, join=True)
         # try to find both columns, if you fail raise error
-        try:
+        #try:
             column_index_left = self.column_names.index(column_name_left)
             column_index_right = table_right.column_names.index(column_name_right)
-        except:
-            raise Exception(f'Columns dont exist in one or both tables.')
+        #except:
+        #    raise Exception(f'Columns dont exist in one or both tables.')
 
         # get the column names of both tables with the table name in front
         # ex. for left -> name becomes left_table_name_name etc
@@ -552,7 +552,7 @@ class Table:
         no_of_ops = 0
         # this code is dumb on purpose... it needs to illustrate the underline technique
         # for each value in left column and right column, if condition, append the corresponding row to the new table
-        
+
         #hasIndex = _has_index(f'{table_right._name}')
         #if hasIndex:
 
