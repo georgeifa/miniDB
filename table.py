1511e7ecd626f7e871b1d824bacba110a35d1836
george ifantis <georgeifa13@gmail.com>
2021-01-09 21:41:18 +0200

    Sort-Merge Fix - to be tested

--- table.py
+++ table.py
@@ -541,6 +541,8 @@ class Table:
         join_table = Table(name=join_table_name, column_names=join_table_colnames, column_types= join_table_coltypes)
 
 
+        # count the number of operations (<,> etc)
+        no_of_ops = 0
         # this code is dumb on purpose... it needs to illustrate the underline technique
         # for each value in left column and right column, if condition, append the corresponding row to the new table
 
