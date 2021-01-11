4754e90daa3c61c8cd16e99a05cb06666858f661
george ifantis <georgeifa13@gmail.com>
2021-01-11 19:38:55 +0200

    added comments

--- misc_for_Joins.py
+++ misc_for_Joins.py
@@ -1,5 +1,5 @@
 def break_when_ascending(operator,left,right):
-    switcher = {
+    switcher = {#   check the  operator and compare the 2 values
         "==": left < right,
         "<": left >= right,
         "<=": left > right,
@@ -8,12 +8,12 @@ def break_when_ascending(operator,left,right):
     }
 
     try:
-        return switcher[operator]
+        return switcher[operator] # return the result of the comparison
     except TypeError:  # if a or b is None (deleted record), python3 raises typerror
         return False
 
 
-def break_when_descending(operator,left,right):
+def break_when_descending(operator,left,right): # works the same way as the above but it is for a descending sorted table
     switcher = {
         "==": left >= right,
         "<": left < right,
