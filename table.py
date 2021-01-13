b18ee2241b09a21ce52b17fcbe1207cafff39350
george ifantis <georgeifa13@gmail.com>
2021-01-13 13:12:03 +0200

    try with a = none

--- table.py
+++ table.py
@@ -558,6 +558,8 @@ class Table:
             #outer join works with a combination of the left and right join
             #ONLY DIFFERENCE when doing the right_join part we dont insert if there is a match. Only when there is not
 
+            a = None
+
             for row_left in self.data:
                 row_null.clear()
                 hasMatch =  False
@@ -571,7 +573,7 @@ class Table:
                 if not hasMatch:
                     for column in table_right.column_types:
                         if column == type(1) or column == type(1.2):
-                            row_null.append(-0)
+                            row_null.append(a)
                         else:
                             row_null.append(None)
                     join_table._insert(row_left + row_null)
@@ -588,7 +590,7 @@ class Table:
                 if not hasMatch:
                     for column in self.column_types:
                         if column == type(1) or column == type(1.2):
-                            row_null.append(-0)
+                            row_null.append(a)
                         else:
                             row_null.append(None)
                     join_table._insert(row_null + row_right)
